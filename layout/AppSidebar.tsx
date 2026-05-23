66cd4fa3c88ca98c9ebbb5bd417c73a00b9d1b27
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-05-23 22:19:49 +0700

    add dark mode

--- layout/AppSidebar.tsx
+++ layout/AppSidebar.tsx
@@ -47,20 +47,13 @@ const AppSidebar: React.FC = () => {
 
   return (
     <aside className={
-      `fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 h-screen transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
-      ${isExpanded || isMobileOpen
-        ? "w-[290px]"
-        : isHovered
-          ? "w-[290px]"
-          : "w-[90px]"
-      }
-      ${isMobileOpen
-        ? "translate-x-0"
-        : "-translate-x-full"
-      }
-      lg:translate-x-0`
-    }
-
+      `fixed mt-16 flex flex-col lg:mt-0 top-0 px-5 left-0 bg-white dark:bg-gray-900 dark:border-gray-800 text-gray-900 transition-all duration-300 ease-in-out z-50 border-r border-gray-200 
+      ${isExpanded || isMobileOpen ? "w-[290px]" : isHovered ? "w-[290px]" : "w-[90px]"}
+      ${isMobileOpen ? "translate-x-0" : "-translate-x-full" } lg:translate-x-0`}
+      style={{
+        height: "100dvh",
+        paddingBottom: "env(safe-area-inset-bottom)",
+      }}
       onMouseEnter={() => {
         if (!isExpanded) {
           setIsHovered(true);
