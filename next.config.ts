f89b800b5ab14afd7d486973ab9d5eca6e9a8922
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-05-27 15:15:00 +0700

    update code

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['10.149.118.70'],
+  allowedDevOrigins: ['10.149.118.70', '10.149.118.214'],
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
