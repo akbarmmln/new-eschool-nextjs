92c3ce4ee2c4d153bdc565030f06b30dd271b3b4
Muhammad Taufik <muhammad.taufik@adira.co.id>
2026-06-30 21:35:42 +0700

    update code

--- app/preview/jurnal/page.tsx
+++ app/preview/jurnal/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import type { Metadata } from "next";
 import PreviewJurnal from '@/components/ui/PreviewJurnal'
 
@@ -9,5 +10,9 @@ export const metadata: Metadata = {
 };
 
 export default function PreviewJurnalPage() {
-  return <PreviewJurnal/>
+  return (
+    <Suspense fallback={<div>Memuat Preview...</div>}>
+      <PreviewJurnal />
+    </Suspense>
+  );
 }
